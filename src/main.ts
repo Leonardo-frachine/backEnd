8340f28d0d6dbc378990d79cb6a60468305d5ab8
Leo <ldfrachine@minha.fag.edu.com>
2025-06-11 19:52:46 -0300

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     allowedHeader: 'Content-Type, Authorization',
   });
   const port = process.env.PORT ?? 3000;
-  await app.listen(port,'0,0,0,0');
+  await app.listen(port,'0.0.0.0');
   
 }
 bootstrap();
